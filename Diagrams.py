cf62a4a87352a5cbf42ef58f48a6a9be8356281b
N1ngawolf3 <lomakin_2003@list.ru>
2024-05-10 14:57:17 +0300

    Animation and Diagrams with main forces

--- Diagrams.py
+++ Diagrams.py
@@ -4,12 +4,15 @@ from config import *
 from math import asin
 
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+forces = fig.add_subplot(1, 1, 1)
+# tang_forces = fig.add_subplot(2, 1, 2)
 
 
 def x(phi):
     return R*(1 - np.cos(phi) + lR/4 * (1 - np.cos(2*phi)))
 
+def beta(phi):
+    return asin(lR * np.sin(phi))
 
 # initializing the data
 
@@ -19,6 +22,7 @@ yj = []
 yfr = []
 ysum = []
 ypow = []
+ysh = []
 #Gas forces
 for angle in x_axis:
     if angle > 2*np.pi:
@@ -46,28 +50,64 @@ for i in range(len(y)):
 
 for i in range(len(y)):
     current_ysum = yfr[i]+y[i]+yj[i]
-    ypow += [current_ysum * np.tan(asin(lR*np.sin(x_axis[i])))]
+    ypow += [current_ysum * np.tan(beta(x_axis[i]))]
 
-ax.grid(True)
-ax.set_title('Диаграмма Сил')
-ax.plot(x_axis, y)
-ax.plot(x_axis, yj)
-ax.plot(x_axis, yfr)
-ax.plot(x_axis, ysum)
-ax.plot(x_axis, ypow)
+for i in range(len(y)):
+    current_ysum = yfr[i] + y[i] + yj[i]
+    ysh += [current_ysum/np.cos(beta(x_axis[i]))]
 
-plt.xlim(0, 2*np.pi)
-plt.ylim(-2900, 6700)
-plt.yticks(np.arange(-3000, 7000, 500))
-plt.xticks(np.arange(0, 2*np.pi+0.006, np.pi/4))
+T = []
+for i in range(len(y)):
+    T += [ysh[i]*np.sin(x_axis[i]+beta(x_axis[i]))]
+
+Tav = [-5911 for _ in range(len(x_axis))]
+
+
+def Tang(phi):
+    return ysh*np.sin(x_axis[i]+beta(x_axis[i]))
+
+
+# Tsum = []
+# for i in range(len(x_axis)):
+#     phism = 2*np.pi/z
+#     Tsum += [ysh[i]*np.sin(x_axis[i]+beta(x_axis[i])) + ysh[i]*np.sin(x_axis[i]+beta(x_axis[i]))]
+
+forces.grid(True)
+forces.set_title('Диаграмма Сил')
+forces.plot(x_axis, y)
+forces.plot(x_axis, yj, linestyle=':')
+forces.plot(x_axis, yfr, linestyle='-')
+forces.plot(x_axis, ysum, linestyle='--')
+forces.plot(x_axis, ypow, ls='-.')
+# forces.plot(x_axis, ysh)
+
+
+# plt.xlim(0, 2*np.pi+0.006)
+# plt.ylim(-2900, 6700)
+# plt.yticks(np.arange(-3000, 7000, 500))
+# plt.xticks(np.arange(0, 2*np.pi+0.006, np.pi/4))
+
+# x_axis.set_xticklabels(labels)
+plt.legend(['Газовые силы', "Силы Инерции", "Силы трения",
+             'Суммарная сила', "Мощность"]
+            )
+
 labels = ['$0$', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$',
           r'$5\pi/4$', r'$3\pi/2$', r'$7\pi/4$', r'$2\pi$']
-ax.set_xticklabels(labels)
-plt.legend(['Газовые силы', "Силы Инерции", "Силы трения",
-            'Суммарная сила', "Мощность"]
-           )
-ax.set_xlabel(r'$\phi$, рад')
-ax.set_ylabel('Силы, Н\n'
+
+forces.set_xlabel(r'$\phi$, рад')
+forces.set_ylabel('Силы, Н\n'
               'Мощность, Вт')
+forces.set_xlim(0, 2*np.pi+0.006)
+forces.set_ylim(-3000, 7000)
+forces.set_xlim(0, 2*np.pi+0.006)
+forces.set_yticks(np.arange(-3000, 7000, 500))
+forces.set_xticks(np.arange(0, 2*np.pi+0.006, np.pi/4))
+forces.set_xticklabels(labels)
+# tang_forces.set_xlabel(r'$\phi$, рад')
+# tang_forces.set_xlim(0, 2*np.pi+0.006)
+# tang_forces.set_xticks(np.arange(0, 2*np.pi+0.006, np.pi/4))
+# tang_forces.set_xticklabels(labels)
+
 plt.grid(True)
 plt.show()
