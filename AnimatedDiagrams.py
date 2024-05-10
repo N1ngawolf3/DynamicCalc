b0624ac32d7939ad7887110269934f4e01e2a0fd
N1ngawolf3 <lomakin_2003@list.ru>
2024-05-10 15:11:39 +0300

    Cosmetics

--- AnimatedDiagrams.py
+++ AnimatedDiagrams.py
@@ -1,16 +1,21 @@
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from math import asin
 import matplotlib.pyplot as plt
 from config import *
-import sys
+
 
 fig, ax = plt.subplots()
 
-x = np.arange(0, 2*np.pi, 0.01)
+x = np.arange(0, 2*np.pi+0.006, 0.01)
 y = np.arange(0, 2*np.pi, 0.01)
 
-line, = ax.plot(x, y)
-phasa = np.arange(0, 2*np.pi, 0.01)
+inertia_diagram, = ax.plot(x, y)
+gas_force_diagram, = ax.plot(x, y)
+friction_force_diagram, = ax.plot(x, y)
+sum_force_diagram, = ax.plot(x, y)
+pow_diagram, = ax.plot(x, y)
+phase = np.arange(0, 2*np.pi, 0.01)
 
 
 def dist(phi):
@@ -33,24 +38,94 @@ def gas_force(angles):
     return y
 
 
-def update_gas_force(frame, line, x):
+def inertia_force(angles):
+    y = []
+    for angle in angles:
+        if angle > 2*np.pi:
+            angle -= 2*np.pi
+        y += [-(2*np.pi*n/60)**2*R *
+              (np.cos(angle)+lR*np.cos(2*angle))*mps]
+    return y
+
+
+def friction_force(angles):
+    y = []
+    for angle in angles:
+        if angle > 2*np.pi:
+            angle -= 2*np.pi
+        if 0 <= angle <= np.pi:
+            y += [-2 / 3 * Nfr / (Cm * z)]
+        elif np.pi < angle <= 2 * np.pi:
+            y += [2 / 3 * Nfr / (Cm * z)]
+    return y
+
+
+def sum_force(angles):
+    y = []
+    yg = gas_force(angles)
+    yi = inertia_force(angles)
+    yfr = friction_force(angles)
+    for i in range(len(x)):
+        y += [yg[i] + yi[i] + yfr[i]]
+    return y
+
+
+def pow(angles):
+    y = []
+    i = 0
+    for el in sum_force(angles):
+        y += [el*np.tan(asin(lR*np.sin(angles[i])))]
+        i += 1
+    return y
+
+
+def update_forces(frame, inertia_diagram,
+                  gas_force_diagram,
+                  friction_force_diagram,
+                  sum_force_diagram,
+                  pow_diagram, x):
     # frame - параметр, который меняется от кадра к кадру
     # в данном случае - это начальная фаза (угол)
-    # line - ссылка на объект Line2D
-    line.set_ydata(gas_force(x+frame))
-    return [line]
+    # line - ссылка на объект gas_force_diagramD
+    inertia_diagram.set_ydata(inertia_force(x + frame))
+    gas_force_diagram.set_ydata(gas_force(x+frame))
+    friction_force_diagram.set_ydata(friction_force(x+frame))
+    sum_force_diagram.set_ydata(sum_force(x+frame))
+    pow_diagram.set_ydata(pow(x+frame))
+    return (inertia_diagram,
+            gas_force_diagram,
+            friction_force_diagram,
+            sum_force_diagram,
+            pow_diagram)
+
 
 
 animation = FuncAnimation(
     fig,                # фигура, где отображается анимация
-    func=update_gas_force,    # функция обновления текущего кадра
-    frames=phasa,       # параметр, меняющийся от кадра к кадру
-    fargs=(line, x),    # дополнительные параметры для функции update_cos
+    func=update_forces,    # функция обновления текущего кадра
+    frames=phase,       # параметр, меняющийся от кадра к кадру
+    fargs=(inertia_diagram,
+           gas_force_diagram,
+           friction_force_diagram,
+           sum_force_diagram,
+           pow_diagram, x),    # дополнительные параметры для функции update_cos
     interval=5,       # задержка между кадрами в мс
     blit=True,          # использовать ли двойную буферизацию
     repeat=True)       # зацикливать ли анимацию
 
+
 plt.xlim(0, 2*np.pi)
-plt.ylim(-1000, 7000)
+plt.ylim(-2900, 6700)
+plt.yticks(np.arange(-3000, 7000, 500))
+plt.xticks(np.arange(0, 2*np.pi+0.006, np.pi/4))
+ax.set_xlabel(r'$\phi$, рад')
+ax.set_ylabel('Силы, Н\n'
+              'Мощность, Вт')
+labels = ['$0$', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$',
+          r'$5\pi/4$', r'$3\pi/2$', r'$7\pi/4$', r'$2\pi$']
+ax.set_xticklabels(labels)
+ax.set_title('Диаграмма Сил')
+plt.legend(['Силы Инерции', "Газовые силы", "Силы трения",
+            'Суммарная сила', "Мощность"])
 plt.grid(True)
 plt.show()
